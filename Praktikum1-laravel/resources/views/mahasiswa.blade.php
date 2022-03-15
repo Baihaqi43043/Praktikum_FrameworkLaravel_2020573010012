fc4968f43f2a5798115d8ea4daf522795edc69cc
baihaqi <Boihaki43@gmail.com>
2022-03-15 12:24:45 +0700

    Commit.

---
+++ Praktikum1-laravel/resources/views/mahasiswa.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('main')
+<div class="border rounded mt-5 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 380px;">
+        <div class="d-flex justify-content-between flex-shrink-0 p-3 link-dark  border-bottom">
+            <h1>Daftar Mahasiswa</h1>
+        </div>
+        <ol>
+            <li>
+                Baihaqi
+            </li>
+            <li>
+                Dzikri
+            </li>
+            <li>
+                Fauzol
+            </li>
+        </ol>
+    </div>
+@endsection
