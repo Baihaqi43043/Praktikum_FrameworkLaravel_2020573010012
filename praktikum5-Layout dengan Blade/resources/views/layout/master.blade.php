91527392e3c3ea8b41dca0f8f3b602701631ff73
baihaqi <Boihaki43@gmail.com>
2022-06-22 14:33:28 +0700

    laravel ui

--- praktikum5-Layout dengan Blade/resources/views/layout/master.blade.php
+++ praktikum5-Layout dengan Blade/resources/views/layout/master.blade.php
@@ -1,47 +1,27 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="{{asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
+	<meta name="author" content="AdminKit">
+	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
+{{-- 
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link rel="shortcut icon" href="{{ asset('img/icons/icon-48x48.png') }}" />
 
-    <title>@yield('title')</title>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ">
-            <a class="nav-link @yield('menuMahasiswa')" href="/mahasiswa">Mahasiswa</a>
-              <a class="nav-link @yield('menuDosen')" href="/dosen">Dosen</a>
-              <a class="nav-link @yield('menuGalery')" href="/galery">Galery</a>
-            </div>
-          </div>
-        </div>
-      </nav>
+	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
+
+	<title>AdminKit Demo - Bootstrap 5 Admin Template</title>
+
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
+</head>
+<body>
+    
       @yield('content')
-      <footer class="bg-dark py-4 text-white mt-4 ">
-        <div class="container text-center">
-           Sistem Informasi Mahasiswa | Copyright @ {{date("Y")}} Teknik Informatika
-        </div>
-    </footer>
-    
-    
-    <!-- Optional JavaScript; choose one of the two! -->
-   
-       <!-- Option 1: Bootstrap Bundle with Popper -->
-       <script src="{{asset('bootstrap/js/bootstrap.bundle.min.js')}}" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-   
-       <!-- Option 2: Separate Popper and Bootstrap JS -->
-       <!--
-       <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-       -->
-     </body>
-   </html>
+      {{-- <script src="js/app.js"></script> --}}
+  </body>
+  </html>
